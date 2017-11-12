a5fbe910e8b1dd1d45d058d43752b44760483730
suxxa <girard.arthur@gmail.com>
2017-11-12 16:46:54 +0100

    [Fix] Test if form is submit added

--- src/BoncoinBundle/Controller/MainController.php
+++ src/BoncoinBundle/Controller/MainController.php
@@ -27,20 +27,24 @@ class MainController extends Controller
     public function indexAction(Request $request)
     {
         $annonceRepository = $this->getDoctrine()->getRepository('BoncoinBundle:Annonce');
+        $annonces = [];
 
         $searchForm = $this->createForm(MySearchType::class);
 
         $searchForm->handleRequest($request);
 
-        if ($searchForm->isValid()) {
-            $annonces = $annonceRepository->findBySearch(
-                $searchForm->getData()['title'],
-                $searchForm->getData()['pmin'],
-                $searchForm->getData()['pmax']
-            );
+        if ($searchForm->isSubmitted()){
+            if ($searchForm->isValid()) {
+                $annonces = $annonceRepository->findBySearch(
+                    $searchForm->getData()['title'],
+                    $searchForm->getData()['pmin'],
+                    $searchForm->getData()['pmax']
+                );
+            }
         } else {
             $annonces = $annonceRepository->findBySearch();
         }
+
 
         return $this->render('BoncoinBundle:Main:index.html.twig', array(
             'annonces' => $annonces,
